e86e4b636828872105defbffe3796ae89741ca59
Antara Newaz <antaranewaz23@gmail.com>
2022-03-13 13:47:44 -0600

    File created

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ *  file: main.cpp
+ *  author: Antara Newaz <anc812@uregina.ca>
+ *  version: 0.1
+ *  date-created: mar-13-2022
+ *  last modified: mar-13-2022
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
