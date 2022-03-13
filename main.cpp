521c29eca51d61232d68c4d7691d54f0ccb21919
Antara Newaz <antaranewaz23@gmail.com>
2022-03-13 14:10:31 -0600

    Added ascendingShakerSort() function

--- main.cpp
+++ main.cpp
@@ -11,8 +11,70 @@
 #include <vector>
 #include <fstream>
 #include <iterator>
+#include <algorithm>
+#include <sstream>
+#include <time.h>
 
 using namespace std;
+
+// shaker sort algorithm - ascending order
+void ascendingShakerSort() {
+    string fileData;
+    vector<int> arr;
+
+    // read from file, and convert and store to vector
+    ifstream readFile("./random_generation.txt");
+
+    while (getline(readFile, fileData)) {
+        int data = stoi(fileData);
+        arr.push_back(data);
+    }
+
+    readFile.close();
+
+    int temp;
+    bool swap = true;
+
+    clock_t startTime = clock(); // clock start time
+
+    // ascending sort
+    while (swap) {
+        swap = false;
+
+        for (int i = 0; i < arr.size()-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                swap = true;
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+
+        if (!swap) break;
+        
+        swap = false;
+
+        for (int i = arr.size()-2; i >= 0; i--) {
+            if (arr[i] > arr[i+1]) {
+                swap = true;
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+        }
+    }
+
+    string timeTaken = to_string((double)(clock() - startTime) / CLOCKS_PER_SEC); // clock end time and total time of execution
+
+    stringstream result;
+    copy(arr.begin(), arr.end(), std::ostream_iterator<int>(result, "\n"));
+
+    // write to file
+    ofstream createFile("./ascending_order.txt");
+    createFile << result.str();
+    createFile << "\nExecution Time: " << timeTaken << "s";
+    createFile.close();
+}
 
 // randomly generate vector elements
 void randomGeneration(int vectorSize) {
@@ -43,6 +105,8 @@ void userPrompt() {
 
 int main() {
     userPrompt();
+
+    ascendingShakerSort();
 
     return 0;
 }
